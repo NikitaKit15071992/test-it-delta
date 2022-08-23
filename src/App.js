279718d08bc528886301d5ac2a5be9094a8f65c6
Никита Попов <nik-popov-92@mail.ru>
2022-08-23 17:14:31 +0300

    test-it-delta

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Cards from './Elements/Cards/cards'
+import Header from './Elements/Header/Header'
+
+function App() {
+	return (
+		<main className='main-wrapper'>
+			<Header />
+			<Cards />
+		</main>
+	)
+}
+
+export default App
